39168a0f934e7f60c52701ac55b90545d51544f8
lmiroslaw <lukasz.miroslaw@pwr.wroc.pl>
2017-11-10 11:08:06 +0100

    Added import

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,10 @@
 from flask import Flask
+import datetime 
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World at " + str(date)
+    return "Hello World at ", datetime.datetime.now()
 
 if __name__ == "__main__":
     application.run()
