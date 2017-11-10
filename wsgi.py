3b4167df622021865e8fa1188ba82a0d689407a2
lmiroslaw <lukasz.miroslaw@pwr.wroc.pl>
2017-11-10 11:41:07 +0100

    Working version

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,7 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     today=date.today()
-    return "Hello World at "
+    return "Hello World at ", str(today)
 
 if __name__ == "__main__":
     application.run()
