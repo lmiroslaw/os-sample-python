b7e2d0927e2ed70595ab8d56a7bf9e2f0f3355fd
lmiroslaw <lukasz.miroslaw@pwr.wroc.pl>
2017-11-10 11:17:18 +0100

    check import

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World at " 
+    return "Hello World at " + time.strftime("%Y-%m-%d %H:%M")
 
 if __name__ == "__main__":
     application.run()
