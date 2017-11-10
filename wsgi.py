a3abff7903def3eab13c9c0d7a66799ba88ec1f5
lmiroslaw <lukasz.miroslaw@pwr.wroc.pl>
2017-11-10 11:28:16 +0100

    small fix on import

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World at %s", datetime.strftime("%Y-%m-%d %H:%M")
+    return "Hello World at %s", %datetime.strftime("%Y-%m-%d %H:%M")
 
 if __name__ == "__main__":
     application.run()
